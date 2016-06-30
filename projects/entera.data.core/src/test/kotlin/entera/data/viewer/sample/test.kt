16422941425b0762abcf685a9689164b85763b92
Benjamin Gudehus <hastebrot@gmail.com>
2016-07-01 00:04:11 +0200

    (chore) Gradle: Initialize core and sample subprojects.

---
+++ projects/entera.data.core/src/test/kotlin/entera/data/viewer/sample/test.kt
@@ -0,0 +1,13 @@
+package entera.data.viewer.sample
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class SampleTest {
+
+    @Test
+    fun test() {
+        assertEquals(5, 2 + 3)
+    }
+
+}
